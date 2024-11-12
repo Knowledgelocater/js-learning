518759bc53066a5756fc77ae53802dcdb680eee1
Kinshuk Shukla <87925952+Knowledgelocater@users.noreply.github.com>
2024-11-12 15:40:28 +0000

    conversions

---
+++ 01_basics/conversions.js
@@ -0,0 +1,13 @@
+let digit = "33";
+let digitAB = "33ab"
+console.log(typeof digit);
+
+let digitInNumber = Number(digit);
+let digitNum = Number(digitAB);
+console.log(typeof digitNum);
+console.log(digitNum);
+
+
+// "33" => 33
+// "33ab" => NAN
+// true =
