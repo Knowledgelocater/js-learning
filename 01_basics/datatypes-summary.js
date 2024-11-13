976a2c28bc49e64ad6044f60c42e330a42f60936
Kinshuk Shukla <87925952+Knowledgelocater@users.noreply.github.com>
2024-11-13 16:01:47 +0000

    Heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -8,12 +8,25 @@
 
 // javascript is dynamically typed language
 
-const heroes = ["shaktiman" , "Kinshuk"];
-let myObj = {
-    name : "Kinshuk",
-    age : 19,
+// const heroes = ["shaktiman" , "Kinshuk"];
+// let myObj = {
+//     name : "Kinshuk",
+//     age : 19,
 
+// }
+// const myFunc = function(){
+//     console.log("Hello World");
+// }
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack(primitive) // heap(non-primitive)
+
+let employeeOne = {
+    email : "kk@gmail.com",
+    phoneNumber : 9988999999,
 }
-const myFunc = function(){
-    console.log("Hello World");
-}
+let employeeTwo = employeeOne;
+employeeTwo.email = "ramesh@gmail.com";
+console.log(employeeOne.email);
+console.log(employeeTwo.email); // heap ke under refrence  milta hai
