0718532ff50785a7a6e63b954c745b8892934d7e
Kinshuk Shukla <87925952+Knowledgelocater@users.noreply.github.com>
2024-11-13 15:52:46 +0000

    Datatype Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,19 @@
+//primitive
+
+// 7 types : number , string , null , undefined , bigint , symbol , boolean
+
+// reference type ( non-primitive )
+
+// arrays , objects, fuction
+
+// javascript is dynamically typed language
+
+const heroes = ["shaktiman" , "Kinshuk"];
+let myObj = {
+    name : "Kinshuk",
+    age : 19,
+
+}
+const myFunc = function(){
+    console.log("Hello World");
+}
