25b94ed425e9f7ad9181a17075848741f8318bea
Kinshuk Shukla <87925952+Knowledgelocater@users.noreply.github.com>
2024-11-19 10:52:07 +0000

    Array

---
+++ 02_basics/arrays.js
@@ -0,0 +1,12 @@
+const myArr = [0,1,2,3,4,5]  // javascript array are resizable
+
+
+
+const myArr2 = new Array("Ram", "shyam");
+console.log(myArr[0]);
+
+myArr.push(6);
+console.log(myArr);
+
+myArr.pop();
+console.log(myArr);
